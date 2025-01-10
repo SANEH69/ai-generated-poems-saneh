b1b2edf86e65d49194894426a0a974a44c2b1853
SANEH69 <sanehthandoh02@gmail.com>
2025-01-10 22:58:21 +0200

    Added more context

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function getPoem(event) {
     event.preventDefault()
     let searchPoemElement = document.querySelector("#search-poem");
     let apiKey = "04d1784de2be03a1bd2o2db8tf6b23e4";
-    let context = "You are an intelligent virtual assistant that has access to many wonderful, heart-warming poems. Please get straight to the poem and do not provide a response unless it is a poem. Please make sure that the poem has a maximum of six lines. Please make sure that each sentence starts in it own line below the first sentence. Please sign '~Saneh🩷' at the end of the poem.";
+    let context = "You are an intelligent virtual assistant that has access to many wonderful, heart-warming poems. Please get straight to the poem and do not provide a response unless it is a poem. Please make sure that the poem has a maximum of six lines. Please do not make it a paragraph. Please make sure that each sentence starts in it own line below the first sentence. Please sign '~Saneh🩷' at the end of the poem and make sure that it is in a separate from the poem.";
     let prompt = `Please tell me a poem about ${searchPoemElement.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
